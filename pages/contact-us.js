8a3c151d3c70b80b751d33cc7882c5266b2f0691
Tarun200999 <tk4977163@gmail.com>
2021-09-14 14:41:17 +0530

    Contact us API integrated

--- pages/contact-us.js
+++ pages/contact-us.js
@@ -1,33 +1,100 @@
-import { Box, Button, Center, Grid, GridItem, Heading, Image, Input, Link, Text, Textarea } from '@chakra-ui/react';
-import React from 'react';
-import Footer from './common/footerr';
-import Navbar from './common/navbar';
+import {
+  Box,
+  Button,
+  Center,
+  Grid,
+  GridItem,
+  Heading,
+  Image,
+  Input,
+  Link,
+  Text,
+  Textarea,
+} from '@chakra-ui/react'
+import { LocalConvenienceStoreOutlined } from '@material-ui/icons'
+import React, { useState } from 'react'
+import Footer from './common/footerr'
+import Navbar from './common/navbar'
 
 function ContactUS() {
-	return (
-		<Box overflowX="hidden">
-			<title>Flywise - Contact us</title>
-			<Navbar outline="Contact US" />
-			<Grid overflow="hidden" maxW="100vw" templateRows="repeat(12, 1fr)" templateColumns="repeat(12, 1fr)">
-				<GridItem
-					zIndex="1"
-					py={[ '2rem', '1rem', '2rem', '3rem', '10rem' ]}
-					pl={[ '0.4rem', '1rem', '3rem', '6rem', '3rem' ]}
-					pr={[ '0.4rem', '1rem', '1rem', '2rem', '3rem' ]}
-					rowSpan={12}
-					colSpan={[ 12, 12, 6, 6, 6 ]}
-					// bg={bg}
-				>
-					<Heading color="rgba(156, 156, 156, 1)" my="4" textAlign={{base:"center",sm:"left"}} fontSize={[ 'lg', 'xl', '2xl', '3xl', '4xl' ]}>
-						CONTACT US
-					</Heading>
-					<Heading my="8" textAlign={{base:"center",sm:"left"}} fontSize={[ 'xl', '2xl', '3xl', '4xl', '5xl' ]} color="#0DB3FB">
-						{`We've got answers if you've got questions!`}
-					</Heading>
-					<Heading  textAlign={{base:"center",sm:"left"}} fontWeight="500" my="8" fontSize={[ 'md', 'lg', 'xl', '2xl', '3xl' ]}>
-						{`Send us your thoughts, questions, compliments, or resume-related haikus! We'd love to hear from you`}
-					</Heading>
-{/*<Link>
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [phone, setPhone] = useState('')
+  const [message, setMessage] = useState('')
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    if (!name || !email || !phone || !message) {
+      alert('All fields required')
+      return
+    }
+    fetch('https://flywisebackend.herokuapp.com/api/contact', {
+      method: 'POST',
+      body: JSON.stringify({
+        name: name,
+        emai: email,
+        phone: phone,
+        message: message,
+      }),
+    })
+      .then((res) => {
+        return res.json()
+      })
+      .then((result) => {
+        console.log(result)
+        setName('')
+        setMessage('')
+        setPhone('')
+        setName('')
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+  return (
+    <Box overflowX="hidden">
+      <title>Flywise - Contact us</title>
+      <Navbar outline="Contact US" />
+      <Grid
+        overflow="hidden"
+        maxW="100vw"
+        templateRows="repeat(12, 1fr)"
+        templateColumns="repeat(12, 1fr)"
+      >
+        <GridItem
+          zIndex="1"
+          py={['2rem', '1rem', '2rem', '3rem', '10rem']}
+          pl={['0.4rem', '1rem', '3rem', '6rem', '3rem']}
+          pr={['0.4rem', '1rem', '1rem', '2rem', '3rem']}
+          rowSpan={12}
+          colSpan={[12, 12, 6, 6, 6]}
+          // bg={bg}
+        >
+          <Heading
+            color="rgba(156, 156, 156, 1)"
+            my="4"
+            textAlign={{ base: 'center', sm: 'left' }}
+            fontSize={['lg', 'xl', '2xl', '3xl', '4xl']}
+          >
+            CONTACT US
+          </Heading>
+          <Heading
+            my="8"
+            textAlign={{ base: 'center', sm: 'left' }}
+            fontSize={['xl', '2xl', '3xl', '4xl', '5xl']}
+            color="#0DB3FB"
+          >
+            {`We've got answers if you've got questions!`}
+          </Heading>
+          <Heading
+            textAlign={{ base: 'center', sm: 'left' }}
+            fontWeight="500"
+            my="8"
+            fontSize={['md', 'lg', 'xl', '2xl', '3xl']}
+          >
+            {`Send us your thoughts, questions, compliments, or resume-related haikus! We'd love to hear from you`}
+          </Heading>
+          {/*<Link>
 						<Button
 							variant={'solid'}
 							size={'lg'}
@@ -54,8 +121,8 @@ function ContactUS() {
 							</Heading>
 						</Button>
 						</Link>*/}
-				</GridItem>
-				{/*<GridItem
+        </GridItem>
+        {/*<GridItem
 					zIndex="1"
 					pr={[ '0.4rem', '1rem', '3rem', '6rem', '12rem' ]}
 					rowSpan={12}
@@ -63,91 +130,108 @@ function ContactUS() {
 				>
 					<Image mt="16" src="/images/contact_us.png" alt="main" />
 				</GridItem>*/}
-				<GridItem
-					// pr={[ '0.4rem', '1rem', '3rem', '6rem', '8rem' ]}
-					rowSpan={12}
-					marginTop={{base:"20px",sm:"0"}}
-					colSpan={[ 12, 12, 6, 6, 6 ]}
-					bg="linear-gradient(180deg, rgba(230, 246, 255, 0.6) 41.47%, rgba(255, 255, 255, 0) 100%)"
-					py={[ '1rem', '1rem', '2rem', '3rem', '10rem' ]}
-					px="10"
-					zIndex="0"
-					transform="translate(0,-3rem)"
-					
-				>
-					<Heading textAlign="center" color="rgba(156, 156, 156, 1)" fontSize="xl">
-						Want more information?
-					</Heading>
-					<Heading fontSize={{base:"22px",sm:"40px"}}  my={{base:"4",sm:"8"}} textAlign="center">
-						Ask us your questions or schedule a meeting
-					</Heading>
-					<Center>
-						<Box w={[ '18rem', '22rem', '36rem', '44rem', '50rem' ]}>
-							<Text mt="8" mb="2">
-								Your name
-							</Text>
-							<Input
-								bg="whiteAlpha.900"
-								borderColor="blackAlpha.500"
-								_hover={{ borderColor: 'blackAlpha.800' }}
-								focusBorderColor="black"
-								placeholder="Your name"
-							/>
-							<Text mt="8" mb="2">
-								Your Email
-							</Text>
-							<Input
-								bg="whiteAlpha.900"
-								borderColor="blackAlpha.500"
-								_hover={{ borderColor: 'blackAlpha.800' }}
-								focusBorderColor="black"
-								placeholder="someone@email.com"
-							/>
-							<Text mt="8" mb="2">
-								Your Number
-							</Text>
-							<Input
-								bg="whiteAlpha.900"
-								borderColor="blackAlpha.500"
-								_hover={{ borderColor: 'blackAlpha.800' }}
-								focusBorderColor="black"
-								placeholder="Your number"
-							/>
-							<Text mt="8" mb="2">
-								Your message
-							</Text>
-							{/* < placeholder="Here is a sample placeholder" /> */}
-							<Textarea
-								bg="whiteAlpha.900"
-								borderColor="blackAlpha.500"
-								_hover={{ borderColor: 'blackAlpha.800' }}
-								resize={'vertical'}
-								focusBorderColor="black"
-								placeholder="Type your message......"
-							/>
-						</Box>
-					</Center>
-					<Center>
-						<Button
-							variant={'solid'}
-							size={'md'}
-							color="white"
-							fontWeight="semibold"
-							px="6"
-							id="gradient"
-							colorScheme="blue"
-							my="19"
-							rounded="full"
-						>
-							Send request
-						</Button>
-					</Center>
-				</GridItem>
-				
-			</Grid>
-			<Footer />
-		</Box>
-	);
+        <GridItem
+          // pr={[ '0.4rem', '1rem', '3rem', '6rem', '8rem' ]}
+          rowSpan={12}
+          marginTop={{ base: '20px', sm: '0' }}
+          colSpan={[12, 12, 6, 6, 6]}
+          bg="linear-gradient(180deg, rgba(230, 246, 255, 0.6) 41.47%, rgba(255, 255, 255, 0) 100%)"
+          py={['1rem', '1rem', '2rem', '3rem', '10rem']}
+          px="10"
+          zIndex="0"
+          transform="translate(0,-3rem)"
+        >
+          <Heading
+            textAlign="center"
+            color="rgba(156, 156, 156, 1)"
+            fontSize="xl"
+          >
+            Want more information?
+          </Heading>
+          <Heading
+            fontSize={{ base: '22px', sm: '40px' }}
+            my={{ base: '4', sm: '8' }}
+            textAlign="center"
+          >
+            Ask us your questions or schedule a meeting
+          </Heading>
+          <Center>
+            <Box w={['18rem', '22rem', '36rem', '44rem', '50rem']}>
+              <Text mt="8" mb="2">
+                Your name
+              </Text>
+              <Input
+                bg="whiteAlpha.900"
+                borderColor="blackAlpha.500"
+                _hover={{ borderColor: 'blackAlpha.800' }}
+                focusBorderColor="black"
+                placeholder="Your name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+              <Text mt="8" mb="2">
+                Your Email
+              </Text>
+              <Input
+                bg="whiteAlpha.900"
+                borderColor="blackAlpha.500"
+                _hover={{ borderColor: 'blackAlpha.800' }}
+                focusBorderColor="black"
+                placeholder="someone@email.com"
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <Text mt="8" mb="2">
+                Your Number
+              </Text>
+              <Input
+                bg="whiteAlpha.900"
+                borderColor="blackAlpha.500"
+                _hover={{ borderColor: 'blackAlpha.800' }}
+                focusBorderColor="black"
+                placeholder="Your number"
+                required
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+              />
+              <Text mt="8" mb="2">
+                Your message
+              </Text>
+              {/* < placeholder="Here is a sample placeholder" /> */}
+              <Textarea
+                bg="whiteAlpha.900"
+                borderColor="blackAlpha.500"
+                _hover={{ borderColor: 'blackAlpha.800' }}
+                resize={'vertical'}
+                focusBorderColor="black"
+                placeholder="Type your message......"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+              />
+            </Box>
+          </Center>
+          <Center>
+            <Button
+              variant={'solid'}
+              size={'md'}
+              color="white"
+              fontWeight="semibold"
+              px="6"
+              id="gradient"
+              colorScheme="blue"
+              my="19"
+              rounded="full"
+              onClick={(e) => onSubmit(e)}
+            >
+              Send request
+            </Button>
+          </Center>
+        </GridItem>
+      </Grid>
+      <Footer />
+    </Box>
+  )
 }
 
-export default ContactUS;
+export default ContactUS
