8ccee003c3aa3b1fcb2280fb1670266a51ee334b
harshit0606 <harshitasr001@gmail.com>
2021-09-23 16:35:59 +0530

    changes

--- content/homeFaqs.js
+++ content/homeFaqs.js
@@ -2,10 +2,10 @@ let faqs = [
   {
     ques: "Why FlyWise?",
     ans: [
-      "Flywise is a systematic guide, tailored for you right from the initial thought of pursuing Masters' to landing your dream tech job in the USA.",
-      "Worried how you could land a tech job without a tech degree ? Worried about expenses ? Issues about convincing family? We got it all!",
-      "Our motto is transparency in support and building your future!" +
-        "Our mission is to be with you until you land your dream job. (Currently, all the study abroad education consultants only helps to get an admit)",
+      "Our career experts at Flywise have a wealth of knowledge to tailor make programs that'll help you plan your journey from the very beginning till the end i.e., from when you decide to pursue Masters to securing that dream job. ",
+      "If you're worried about landing a technical job without technical background/ experience or about education loans & expenses, we have all the answers and guidance for you. We've got you covered!",
+      "We at Flywise strongly believe in our motto - Transparency in support and building your future!",
+      "Unlike other consultants who offer support only until your university admission is confirmed, Flywise's only mission is to empower and support you until you land your dream job.",
     ],
   },
   {
@@ -46,14 +46,14 @@ let faqs = [
   {
     ques: "Does spring admit affect job opportunities?    ",
     ans: [
-      "If you are planning for a 2 year course, spring admit won’t make any difference. If you want to finish    your masters in 1.5 years, you will lose an opportunity to get summer internships becuase you will be done with only one semester by that time (you need to finish 2 semesters to do internship).      ",
+      "If you are planning for a 2 year course, spring admit won’t make any difference. If you want to finish your masters in 1.5 years, you will lose an opportunity to get summer internships because you will be done with only one semester by that time and you need to finish 2 semesters to be eligible for CPT(Curricular Practical Training), a government authorization to do summer internship.",
       "The no.of jobs available and opportunities won’t change at all.      ",
     ],
   },
   {
     ques: "I did my B.tech in Non-CS stream, can I pursue masters in computer science/data science?    ",
     ans: [
-      "YES. Few of mentors personally have gone through the process and we will be very much happy to guide you through it.      ",
+      "Yes. Some of our mentors have personally pursued computer science/data science related courses without similar educational backgrounds. They will guide you through the process and answer any questions you may have regarding the same.",
     ],
   },
   {
@@ -65,8 +65,7 @@ let faqs = [
   {
     ques: "When is the right time to pursue ms? right after BTech or have an experience then pursue ms?    ",
     ans: [
-      "Often it varies case by case. If you get an offer from a top product based company you can experience it for a year or two, in general it is advisable to do masters right after your graduation because time does matter and you will start meaning in dollars early if you come to the USA early.      ",
-      "Note: US companies won’t consider your experience at any services based companies back in India.      ",
+      "This varies based on multiple factors and we will provide top class advice based on your educational background, experience etc., However, it is advised to pursue Masters right after Graduation as Tech giants in US may/may not consider your work experience at service based companies in India. The sooner you get to the US, the faster you can start earning a steady income at your dream company.",
     ],
   },
   {
