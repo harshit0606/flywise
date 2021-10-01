53a07e48567f0c2babe2f6bc069cbf84214c893f
harshit0606 <harshitasr001@gmail.com>
2021-10-01 13:02:37 +0530

    content changes

--- pages/profile-evaluation/index.js
+++ pages/profile-evaluation/index.js
@@ -1177,10 +1177,11 @@ function One() {
                   </Text>
                 </GridItem>
                 {[
-                  "Spring 2022",
+                  
                   "Fall 2022",
                   "Spring 2023",
                   "Fall 2023",
+                  "Spring 2024",
                   "Other",
                 ].map((i, idx) => (
                   <GridItem
